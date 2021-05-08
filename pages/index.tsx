660aa172a3bf7dfc9c37e2ad1bb87095893c2a78
saadraj <saadnbiuraj@gmail.com>
2021-05-09 01:21:05 +0600

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { Box, Container, TextField, Typography } from "@material-ui/core";
+import {
+    Box,
+    Button,
+    Container,
+    TextField,
+    Typography,
+} from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
 import firebase, { fireStore } from "../Firebase";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -11,8 +18,14 @@ const useStyles = makeStyles((theme: Theme) =>
             height: "100vh",
         },
         form: {
-            width: 500,
-            paddingBottom: theme.spacing(5),
+            width: "50vw",
+            padding: theme.spacing(5),
+        },
+        link: {
+            width: "50vw",
+            overflowX: "auto",
+            backgroundColor: "#e7e7e7",
+            padding: theme.spacing(5),
         },
     })
 );
@@ -25,12 +38,19 @@ function uniqueId() {
     });
 }
 
-const urlValidator = /^(http[s]?:\/\/){0,1}(w{3,3}\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/;
+    const urlValidator = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
 
 export default function index() {
     const classes = useStyles();
     const [input, setInput] = useState("");
     const [total, setTotal] = useState(0);
+    const [copy, setCopy] = useState(false);
     const [error, setError] = useState("");
     const [shortUrl, setShortUrl] = useState("");
 
@@ -40,15 +60,12 @@ export default function index() {
         });
     }, []);
 
-    const submit = (e) => {
+    const submit =  (e) => {
         e.preventDefault();
         if (input === "") {
             setError("Enter a URL");
         } else {
-            if (!urlValidator.test(input)) {
-                setError("Plaease Enter a valid URL");
-                return;
-            }
+            if(urlValidator.test(input)) {
             const id = uniqueId();
             const url = `https://${window.location.hostname}/url/${id}`;
             fireStore
@@ -66,7 +83,15 @@ export default function index() {
                 .catch((e) => {
                     setError(e.message);
                 });
+            } else {
+                setError("Please Enter a valid URL");
+            }
         }
+    };
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(shortUrl);
+        setCopy(true);
     };
 
     return (
@@ -97,17 +122,32 @@ export default function index() {
                     </form>
                     {shortUrl && (
                         <Typography align="center" variant="h6" paragraph>
-                            Sort URL ={" "}
-                            <a href={shortUrl} target="_blank">
-                                <Typography
-                                    align="center"
-                                    variant="h6"
-                                    component="span"
-                                    paragraph
-                                >
-                                    {shortUrl}
-                                </Typography>
-                            </a>
+                            <Button
+                                color={copy ? "secondary" : "primary"}
+                                onClick={copyToClipboard}
+                            >
+                                {copy ? "Copied" : "Copy to Clipboard"}
+                            </Button>
+                            <Typography
+                                align="center"
+                                variant="h6"
+                                paragraph
+                                className={classes.link}
+                                onClick={() => {
+                                    navigator.clipboard.writeText(shortUrl);
+                                }}
+                            >
+                                <a href={shortUrl} target="_blank">
+                                    <Typography
+                                        align="center"
+                                        variant="h6"
+                                        component="span"
+                                        paragraph
+                                    >
+                                        {shortUrl}
+                                    </Typography>
+                                </a>
+                            </Typography>
                         </Typography>
                     )}
                 </Box>
